fb5f6c1b29ff4a7209b455fbcf8b2da03ad0b31a
kadoka <tomiya3499@gmail.com>
2026-09-14 07:39:31 +0900

    Export generic simulation APIs

--- python/math_sim/simulations/__init__.py
+++ python/math_sim/simulations/__init__.py
@@ -1,5 +1,25 @@
 """Simulation implementations exposed to the Python parent application."""
 
-from .monte_carlo import MonteCarloPiResult, estimate_pi
+from .monte_carlo import (
+    MonteCarloIntegralResult,
+    MonteCarloPiResult,
+    compile_expression,
+    estimate_pi,
+    integrate_expression,
+    monte_carlo_integrate,
+)
+from .random_tree import Branch, BranchRule, Segment, binary_random_rule, generate_tree
 
-__all__ = ["MonteCarloPiResult", "estimate_pi"]
+__all__ = [
+    "MonteCarloIntegralResult",
+    "MonteCarloPiResult",
+    "compile_expression",
+    "estimate_pi",
+    "integrate_expression",
+    "monte_carlo_integrate",
+    "Branch",
+    "BranchRule",
+    "Segment",
+    "binary_random_rule",
+    "generate_tree",
+]
