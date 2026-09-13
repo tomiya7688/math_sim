6a9a8e110d6f9ffbdb98b27307a4b7c5e10a97bb
kadoka <tomiya3499@gmail.com>
2026-09-14 08:09:59 +0900

    Export user-function security checker API

--- python/math_sim/user_functions/__init__.py
+++ python/math_sim/user_functions/__init__.py
@@ -2,14 +2,28 @@ from math_sim.user_functions.catalog import FunctionCatalog
 from math_sim.user_functions.contracts import FunctionResponse, FunctionSpec
 from math_sim.user_functions.integration import UserFunctionIntegralResult, monte_carlo_integrate_user_function
 from math_sim.user_functions.providers import create_provider
+from math_sim.user_functions.security import (
+    SecurityFinding,
+    SecurityReport,
+    check_function_spec,
+    check_native_file,
+    check_python_file,
+    enforce_security,
+)
 from math_sim.user_functions.storage import load_spec, save_spec
 
 __all__ = [
     "FunctionCatalog",
     "FunctionResponse",
     "FunctionSpec",
+    "SecurityFinding",
+    "SecurityReport",
     "UserFunctionIntegralResult",
+    "check_function_spec",
+    "check_native_file",
+    "check_python_file",
     "create_provider",
+    "enforce_security",
     "load_spec",
     "monte_carlo_integrate_user_function",
     "save_spec",
