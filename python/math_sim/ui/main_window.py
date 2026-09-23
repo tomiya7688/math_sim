6d7ed9e480f9646c07310764b174ea83005c415f
kadoka <tomiya3499@gmail.com>
2026-09-23 14:42:06 +0900

    Compose maze generator race object directly

--- python/math_sim/ui/main_window.py
+++ python/math_sim/ui/main_window.py
@@ -6,7 +6,7 @@ from math_sim.application import ApplicationServices
 from math_sim.catalog import REGISTRY
 from math_sim.ui import theme
 from math_sim.ui.catalog_page import LearningCatalogPage
-from math_sim.ui.maze_generator_race_page import build_maze_generator_race_page
+from math_sim.ui.maze_generator_race_page import MazeGeneratorRacePage
 from math_sim.ui.maze_page import build_maze_page
 from math_sim.ui.mlp_page import MlpPage
 from math_sim.ui.monte_carlo_page import MonteCarloPage
@@ -122,8 +122,7 @@ class MainWindow(tk.Tk):
             ),
             "pathfinding": PathfindingRouterPage(self, self.page_host),
             "maze": build_maze_page(self, self.page_host),
-            "maze_generator_race": build_maze_generator_race_page(
-                self,
+            "maze_generator_race": MazeGeneratorRacePage(
                 self.page_host,
             ),
         }
