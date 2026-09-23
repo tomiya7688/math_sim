0908a38614dfcd25d7c7d4eb051a4a8e1dc153d7
kadoka <tomiya3499@gmail.com>
2026-09-23 12:20:44 +0900

    Move maze options out of engine adapter

---
+++ python/math_sim/application/maze_catalog.py
@@ -0,0 +1,22 @@
+GENERATORS = (
+    "backtracker",
+    "prim",
+    "kruskal",
+    "binary_tree",
+    "sidewinder",
+    "growing_tree",
+    "aldous_broder",
+    "wilson",
+)
+
+SOLVERS = (
+    "bfs",
+    "bidirectional_bfs",
+    "dfs",
+    "astar",
+    "greedy",
+    "left_hand",
+    "right_hand",
+    "dead_end",
+    "random_mouse",
+)
